663e92c2ecb26823c82f38fe7b8cf75ea880a5a7
chayan-1906 <chayan19062000@gmail.com>
2025-09-02 19:46:27 +0530

    fix: correct bookmark filtering and status in companion queries

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -23,7 +23,7 @@ type Companion = Models.DocumentList<Models.Document> & {
 	subject: Subject;
 	topic: string;
 	duration: number;
-	bookmarked: boolean;
+	isBookmarked: boolean;
 };
 
 interface CreateCompanion {
